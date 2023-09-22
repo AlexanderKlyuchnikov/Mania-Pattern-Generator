9f0792974ce587f6f5414694581d29f8c9f0de95
AlexanderKlyuchnikov <AlexanderK1707@yandex.ru>
2023-09-22 22:59:01 +0300

    Init commit

---
+++ Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace mpg
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
