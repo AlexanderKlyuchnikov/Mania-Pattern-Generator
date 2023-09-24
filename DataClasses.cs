31f87f69319ed5564242bb2ea2789bf753778709
AlexanderKlyuchnikov <AlexanderK1707@yandex.ru>
2023-09-24 23:39:13 +0300

    Add DefString method in NamedLine class

--- DataClasses.cs
+++ DataClasses.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace mpg
@@ -18,11 +19,39 @@ namespace mpg
         public NamedLineTypes type = NamedLineTypes.NoteCount;
         public string value = "";
         public string index = "";
+        private Random _rnd = new Random();
         public NamedLine(NamedLineTypes type, string value, string index)
         {
             this.type = type;
             this.value = value;
             this.index = index;
+        }
+        public string DefString(string prev)
+        {
+            if (this.type == NamedLineTypes.NoteCount)
+            {
+                int val = int.Parse(this.value);
+                List<int> avpos = new List<int>();
+                char[] result = new char[4] {'0','0','0','0'};
+                for (int i = 0; i < 4; i++)
+                {
+                    if (prev[i] == '0')
+                    {
+                        avpos.Add(i);
+                        //Console.WriteLine(i);
+                    }
+                }
+                for (int i = 0; i < val; i++)
+                {
+                    Random rndhelp = new Random(this._rnd.Next() + i);
+                    int setpos = rndhelp.Next(0, avpos.Count);
+                    result[avpos[setpos]] = '1';
+                    avpos.RemoveAt(setpos);
+                }
+                return new string(result);
+            }
+            else
+                return "0000";
         }
     }
 }
